899d4393387cb37366c8bfbb1288258e4b697312
pjbrof <pjbrof@gmail.com>
2024-07-30 20:13:31 -0400

    runway email feature

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-const https = require("https");
+require("dotenv").config();
 const express = require("express");
+const cron = require("node-cron");
+const nodemailer = require("nodemailer");
+
 const app = express();
 const port = 3000;
 
-// https://aviationweather.gov/api/data/metar?ids=KBED&format=raw&taf=false&hours=0
-/*
-curl -X 'GET' \
-  'https://aviationweather.gov/api/data/metar?ids=KBED&format=raw&taf=false&hours=0' \
-*/
-// -H 'accept: */*'
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailgun.org",
+  port: 587,
+  secure: false, // use SSL
+  auth: {
+    user: process.env.MAILGUN_USER,
+    pass: process.env.MAILGUN_PASS,
+    api_key: process.env.MAILGUN_DOMAIN,
+    domain: process.env.MAILGUN_API_KEY,
+  },
+});
 
-const getCurrentWx = async () => {
+const notifyRunwayInUse = async () => {
+  const currentWx = await getCurrentWxJSON();
+  if (currentWx.wdir >= 70 && currentWx.wdir <= 160) {
+    transporter.sendMail(
+      {
+        from: process.env.MAILGUN_USER,
+        to: process.env.SEND_TO_EMAIL,
+        subject: "Runway 11 in use",
+        text: currentWx.rawOb,
+      },
+      (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      }
+    );
+  }
+};
+
+const getCurrentWxRaw = async () => {
   const res = await fetch(
     "https://aviationweather.gov/api/data/metar?ids=KBED&format=raw&taf=false&hours=0"
   );
@@ -18,11 +47,23 @@ const getCurrentWx = async () => {
   return wx;
 };
 
-app.get("/", async (req, res) => {
-  const wx = await getCurrentWx();
+const getCurrentWxJSON = async () => {
+  const res = await fetch(
+    "https://aviationweather.gov/api/data/metar?ids=KBED&format=json&taf=false&hours=0"
+  );
+  const wx = await res.json();
+  return wx[0];
+};
+
+app.get("/", async (_, res) => {
+  const wx = await getCurrentWxRaw();
   res.send(wx);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Weather app listening on port ${port}`);
+});
+
+cron.schedule("*/30 * * * *", async () => {
+  await notifyRunwayInUse();
 });
